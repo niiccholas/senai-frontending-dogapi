e158e2c407375c669d9d3205ec36d1df5831137c
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@CFP127ALUNO.LOCAL>
2025-02-20 15:35:37 -0300

    projeto finalizado

---
+++ dogapi.js
@@ -0,0 +1,27 @@
+'use strict'
+
+async function pesquisarFotos(raca){
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.message
+}
+
+function criaImagem(link){
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img')
+    novaImg.src = link
+    galeria.appendChild(novaImg)
+}
+
+async function preencherFotos(){
+    const raca = document.getElementById('raca').value
+    const fotos = await pesquisarFotos(raca)
+    const galeria = document.getElementById('galeria')
+    galeria.replaceChildren('')
+
+    fotos.forEach(criaImagem)
+}
+
+document.getElementById('pesquisar').addEventListener("click", preencherFotos)
